c10dd774612861c14758398bc412555b6ee9b98e
James Santiago <jsantiago@invaluable.com>
2018-12-03 09:16:31 -0500

    firstTest

--- src/test/java/SampleAPITests.java
+++ src/test/java/SampleAPITests.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class SampleAPITests {
+
+
+
+    @Test(groups={"API}"})
+    public void firstTest() {
+
+        given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/1")
+                .then()
+                .assertThat()
+                .statusCode(200);
+
+    }
+
 }
